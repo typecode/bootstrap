6d8256d012f37df33cabac51f5841846c81f2551
Andrew Mahon (Type/Code) <andrew@typeslashcode.com>
2010-10-22 11:51:32 -0400

    removed ohmyzsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,7 @@
 cd "`dirname "$0"`"
 
 sudo apt-get update
-sudo apt-get install zsh
+sudo apt-get -f install zsh git-core
+git clone git://github.com/typecode/bootstrap.git
+cp bootstrap/home/.zshrc ~/.zshrc
+cp bootstrap/home/.oh-my-zsh ~/.oh-my-zsh
