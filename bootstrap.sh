a27284a1c3c173c3bd48f0333f3a545adf74df4e
Andrew Mahon (Type/Code) <andrew@typeslashcode.com>
2010-10-22 13:08:15 -0400

    readme changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 cd "`dirname "$0"`"
 
-sudo passwd ubuntu
+echo "Please enter a password for this user"
+
+sudo passwd
 
 sudo apt-get update
-sudo apt-get -f install zsh git-core
+sudo apt-get -y install zsh git-core
 git clone git://github.com/typecode/bootstrap.git
 cp bootstrap/home/.zshrc ~/.zshrc
 git clone http://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh
 cp bootstrap/home/prose.zsh-theme ~/.oh-my-zsh/themes
 
 chsh -s /bin/zsh
-
-exit
+zsh
