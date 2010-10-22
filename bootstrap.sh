16bed3db835f4842132182fbb25c40943e650b5b
Andrew Mahon (Type/Code) <andrew@typeslashcode.com>
2010-10-22 12:01:20 -0400

    more changes to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 cd "`dirname "$0"`"
 
+sudo passwd ubuntu
+
 sudo apt-get update
 sudo apt-get -f install zsh git-core
 git clone git://github.com/typecode/bootstrap.git
 cp bootstrap/home/.zshrc ~/.zshrc
-cp bootstrap/home/.oh-my-zsh ~/.oh-my-zsh
+git clone http://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh
+cp bootstrap/home/prose.zsh-theme ~/.oh-my-zsh/themes
+
+chsh -s /bin/zsh
+
+exit
